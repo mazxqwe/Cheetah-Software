58e0a8ca5c691c7ae0271e57364dc7773283a4bf
Sorokonog <roman@voltbro.com>
2021-10-18 18:30:05 +0300

    LCM patched

--- user/MIT_Controller/Controllers/BalanceController/BalanceController.cpp
+++ user/MIT_Controller/Controllers/BalanceController/BalanceController.cpp
@@ -32,7 +32,7 @@ BalanceController::BalanceController()
 
   QPFinished = false;
 
-  lcm = new lcm::LCM("udpm://239.255.76.67:7667?ttl=1");
+  lcm = new lcm::LCM("udpm://239.255.76.67:7667?ttl=0");
   if (lcm->good()) {
     printf("LCM IN BALANCE CONTROL INITIALIZED\n");
   } else {
--- user/MIT_Controller/Controllers/BalanceController/BalanceControllerVBL.cpp
+++ user/MIT_Controller/Controllers/BalanceController/BalanceControllerVBL.cpp
@@ -33,7 +33,7 @@ BalanceControllerVBL::BalanceControllerVBL() :  QProblemObj_qpOASES(vblNUM_VARIA
    QPFinished = false;
 
 
-   lcm = new lcm::LCM("udpm://239.255.76.67:7667?ttl=1");
+   lcm = new lcm::LCM("udpm://239.255.76.67:7667?ttl=0");
    if (lcm->good())
    {
       printf("LCM IN BALANCE CONTROL INITIALIZED\n");
